0e9238073cb4398b333b61aecd4e28a0aa7e376c
mr. plane <123030498+treyplane@users.noreply.github.com>
2023-04-05 10:01:33 -0500

    Create textures.js

---
+++ textures.js
@@ -0,0 +1,23 @@
+import { TextureLoader } from 'three'
+
+import {
+  dirtimg,
+  grassimg,
+  glassimg,
+  logimg,
+  woodimg
+} from './img'
+
+const dirtTexture = new TextureLoader(dirtimg)
+const logTexture = new TextureLoader(logimg)
+const groundTexture = new TextureLoader(grassimg)
+const glassTexture = new TextureLoader(glassimg)
+const woodTexture = new TextureLoader(woodimg)
+
+export {
+  dirtTexture,
+  logTexture,
+  groundTexture,
+  glassTexture,
+  woodTexture
+}
