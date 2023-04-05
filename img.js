f037c318d94464509235ecaf20a1e77753ce3aa3
mr. plane <123030498+treyplane@users.noreply.github.com>
2023-04-05 09:37:51 -0500

    Create img.js

---
+++ img.js
@@ -0,0 +1,13 @@
+import dirtimg from './pixil-frame-0 (4).png';
+import grassimg from './pixil-frame-0 (3).png';
+import glassimg from './tf.png';
+import logimg from './f.jpeg';
+import woodimg from './th.jpeg';
+
+export {
+  dirtimg
+  grassimg
+  glassimg
+  logimg
+  woodimg
+}
